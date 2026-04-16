7a6827b8dfa3c86cfb5546548f773aaab5034f6c
ninaweng6716 <146882360+ninaweng6716@users.noreply.github.com>
2026-04-16 12:23:47 -0700

    updated info.js

--- src/data/info.js
+++ src/data/info.js
@@ -1,10 +1,6 @@
 export const info = {
-  name:     'Your Name',
-  initials: 'YN',
+  name:     'Nina Weng',
+  initials: 'NW',
   role:     'Front-End Web Developer',
-  location: 'Vancouver, BC',
-  email:    'hello@yourname.dev',
-  linkedin: 'linkedin.com/in/yourname',
-  github:   'github.com/yourname',
-  available: true,   // toggles the "Available for work" chip
+  location: 'Vancouver, BC'
 }
