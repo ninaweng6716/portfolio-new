c4cd33958da6f5cbd6799fcdba1c8ee7e215796c
ninaweng6716 <146882360+ninaweng6716@users.noreply.github.com>
2026-04-16 12:41:35 -0700

    removed weather based bg

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ import About    from './components/About'
 import Projects from './components/Projects'
 import Contact  from './components/Contact'
 import Footer   from './components/Footer'
-import Background from './components/Background'
 
 export default function App() {
   useReveal()   // sets up IntersectionObserver for .reveal elements
 
   return (
     <>
-      <Background />
       <Nav />
       <main>
         <Hero />
