a01f1845a06e89c487e4b74a35ab499a4c278c14
ninaweng6716 <146882360+ninaweng6716@users.noreply.github.com>
2026-04-21 12:22:27 -0700

    changed import to index.jsx for climbingGyms

--- src/components/about/AboutGyms.jsx
+++ src/components/about/AboutGyms.jsx
@@ -1,6 +1,6 @@
 import { lazy, Suspense } from "react"
 
-const ClimbingGyms = lazy(() => import("../climbingGyms"))
+const ClimbingGyms = lazy(() => import("../climbingGyms/index.jsx"))
 
 export default function AboutGyms({ coords }) {
   if (!coords?.lat || !coords?.lng) return null
