782ff9ae121dc550e4b24750fc17673edc477f33
ninaweng6716 <146882360+ninaweng6716@users.noreply.github.com>
2026-04-21 10:50:00 -0700

    fonts now self hosted

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import { writeFileSync, mkdirSync } from 'fs'
+
+function sitemapPlugin() {
+  return {
+    name: 'generate-sitemap',
+    closeBundle() {
+      mkdirSync('dist', { recursive: true })
+
+      const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://ninaweng.com/</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <priority>1.0</priority>
+  </url>
+</urlset>`
+      writeFileSync('dist/sitemap.xml', sitemap)
+    }
+  }
+}
 
 export default defineConfig({
   base: '/',
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), sitemapPlugin()],
   optimizeDeps: {
     esbuildOptions: {
       loader: {
