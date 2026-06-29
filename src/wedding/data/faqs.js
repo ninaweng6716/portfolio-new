e7501601c6424ee63e810cbb3bce153ed949fe71
ninaweng6716 <146882360+ninaweng6716@users.noreply.github.com>
2026-06-29 16:22:06 -0700

    added more faqs

--- src/wedding/data/faqs.js
+++ src/wedding/data/faqs.js
@@ -8,12 +8,12 @@ export const FAQS = [
     content: "Please arrive at least 15 minutes early. The doors will close promptly at 4:00 PM.",
   },
   {
-    q: "Is the ceremony indoors or outdoors?",
-    content: "The ceremony is mostly outdoors and takes place at the Scholar’s Study and Courtyard in the Dr. Sun Yat-Sen Garden. In the event of rain, the ceremony will be moved indoors to a different section of the garden.",
+    q: "Is there parking at the ceremony?",
+    content: "Paid parking (EasyPark) is available at <a class=\"text-weddingTq border-b border-weddingTq/30 transition-colors duration-200 hover:text-weddingTq-dim hover:border-weddingTq\" href=\"https://www.google.com/maps/place/EasyPark+-+Lot+7/@49.2792268,-123.1017783,20z/data=!3m1!5s0x5486717a5ffead83:0xe0347a73a4a6068f!4m14!1m7!3m6!1s0x5486717aa76acfd7:0x4160972be54a477a!2sChinatown+Plaza!8m2!3d49.2791503!4d-123.10184!16s%2Fg%2F11dzstkb8l!3m5!1s0x5486717f978e25d1:0xb8f953635ed01c91!8m2!3d49.2791133!4d-123.1012439!16s%2Fg%2F1tfbbvj_?entry=ttu&g_ep=EgoyMDI2MDYyNC4wIKXMDSoASAFQAw%3D%3D\" target=\"_blank\" rel=\"noopener noreferrer\">Chinatown Plaza</a>, a 5-minute walk from the garden.",
   },
   {
-    q: "Is there parking at the ceremony?",
-    content: "Paid parking (EasyPark) is available at <a class=\"wedding-faq-link\" href=\"https://www.google.com/maps/place/EasyPark+-+Lot+7/@49.2792268,-123.1017783,20z/data=!3m1!5s0x5486717a5ffead83:0xe0347a73a4a6068f!4m14!1m7!3m6!1s0x5486717aa76acfd7:0x4160972be54a477a!2sChinatown+Plaza!8m2!3d49.2791503!4d-123.10184!16s%2Fg%2F11dzstkb8l!3m5!1s0x5486717f978e25d1:0xb8f953635ed01c91!8m2!3d49.2791133!4d-123.1012439!16s%2Fg%2F1tfbbvj_?entry=ttu&g_ep=EgoyMDI2MDYyNC4wIKXMDSoASAFQAw%3D%3D\" target=\"_blank\" rel=\"noopener noreferrer\">Chinatown Plaza</a>. It is a 5-minute walk to the garden from there.",
+    q: "Is the ceremony indoors or outdoors?",
+    content: "The ceremony is mostly outdoors and takes place at the Scholar’s Study and Courtyard in the Dr. Sun Yat-Sen Garden. In the event of rain, the ceremony will be moved indoors to a different section of the garden.",
   },
   {
     q: "What does the ceremony include and how long is it?",
@@ -33,14 +33,14 @@ export const FAQS = [
   },
   {
     q: "Is there parking at the reception?",
-    content: "Underground parking is available <a class=\"wedding-faq-link\" href=\"https://www.google.com/maps/place/49%C2%B015'02.4%22N+123%C2%B006'01.3%22W/@49.2506709,-123.1010097,19z/data=!3m1!4b1!4m4!3m3!8m2!3d49.25067!4d-123.100366?entry=ttu&g_ep=EgoyMDI2MDYyNC4wIKXMDSoASAFQAw%3D%3D\" target=\"_blank\" rel=\"noopener noreferrer\">behind the restaurant</a> and shared with other restaurant goers; carpooling is appreciated.",
+    content: "Underground parking is available <a class=\"text-weddingTq border-b border-weddingTq/30 transition-colors duration-200 hover:text-weddingTq-dim hover:border-weddingTq\" href=\"https://www.google.com/maps/place/49%C2%B015'02.4%22N+123%C2%B006'01.3%22W/@49.2506709,-123.1010097,19z/data=!3m1!4b1!4m4!3m3!8m2!3d49.25067!4d-123.100366?entry=ttu&g_ep=EgoyMDI2MDYyNC4wIKXMDSoASAFQAw%3D%3D\" target=\"_blank\" rel=\"noopener noreferrer\">behind the restaurant</a> and shared with other restaurant goers; carpooling is appreciated.",
   },
   {
     q: "Will there be food and drinks at the reception?",
     content: "Yes. Drinks and a full Chinese-style banquet will be served at Sun Sui Wah.",
   },
   {
-    q: "What if I have a dietary restriction?",
-    content: "Please note any dietary needs in your RSVP.",
-  },
+    q: "What if I have a dietary restriction, preference, or a  food allergy?",
+    content: "Please note any dietary needs in your RSVP. As this is a Chinese banquet-style dinner with shared dishes, individual dietary substitutions are limited, but we will do our best to accommodate where possible.",
+  }
 ]
--- src/wedding/data/weddingDetails.js
+++ src/wedding/data/weddingDetails.js
@@ -13,7 +13,7 @@ export const DETAILS = [
     title: "Reception",
     main: "Sun Sui Wah (Main St.)",
     meta: "Begins at 7:00 PM",
-    body: "Please arrive at least 15 minutes early for check-in.",
+    body: "Please check in at the reception desk upon arrival.",
     link: {
       label: "Get Directions",
       href: "https://www.google.com/maps/place/Sun+Sui+Wah+Seafood+Restaurant/@49.250525,-123.1031822,17z/data=!3m2!4b1!5s0x548673fa1851198f:0xdc90055a228eb138!4m6!3m5!1s0x5486752e706a9a31:0x89c713b9c372f1fd!8m2!3d49.250525!4d-123.1006073!16s%2Fm%2F04yqwln?entry=ttu&g_ep=EgoyMDI2MDQyOC4wIKXMDSoASAFQAw%3D%3D",
